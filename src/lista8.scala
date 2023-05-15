6da83acd9acfbf77e5387d441a0da5617b6e1c17
Kamila Sproska <kamila.sproska@gmail.com>
2023-05-15 16:27:06 +0200

    improving tests

--- src/lista8.scala
+++ src/lista8.scala
@@ -39,17 +39,16 @@ class QueueMut[E: ClassTag](val size: Int = 1000) extends MyQueue[E] {
     endInx = (endInx + 1) % array_size
 
   override def dequeue: Unit =
-    if !isEmpty then {
-      //      queue(firstInx) = Nil.asInstanceOf[E]
+    if !isEmpty then
+//      queue(firstInx) = ClassTag.Null.asInstanceOf[E]
       firstInx = (firstInx + 1) % array_size
-    }
 
   override def first: E =
     if isEmpty then throw new NoSuchElementException("Queue is empty")
     else queue(firstInx)
 
   override def toString: String =
-    s"${queue.mkString("Array(", ", ", ")")}; firstElem: $first; firstInx: $firstInx; endInx: $endInx, size: ${array_size - 1}+1"
+    s"${queue.mkString("Array(", ", ", ")")}; firstInx: $firstInx; endInx: $endInx, size: ${array_size - 1}+1"
 }
 
 // b
@@ -68,20 +67,27 @@ object QueueMut {
 object lista8 {
   def main(args: Array[String]): Unit = {
     val queue = QueueMut(1, 2, 3)
+    println(queue)
     println(queue.isFull)
     println(queue.first == 1)
     queue.dequeue
+    println(queue)
     println(!queue.isFull)
     println(!queue.isEmpty)
     println(queue.first == 2)
     queue.dequeue
+    println(queue)
     println(queue.first == 3)
     queue.dequeue
+    println(queue)
     println(queue.isEmpty)
     queue.enqueue(4)
+    println(queue)
     println(queue.first == 4)
     queue.enqueue(5)
+    println(queue)
     val queueEmpty = QueueMut.empty[Int](10)
+    println(queueEmpty)
     println(queueEmpty.isEmpty)
   }
 }
